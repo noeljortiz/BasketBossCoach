0831f16b0e4f0db3dabdf4f9c40eb6503f0ff78c
noelortiz <noelo12130613@gmail.com>
2023-11-16 09:37:44 -0600

    static vars in rsoter

--- BasketBossCoach/ViewControllerRoster.swift
+++ BasketBossCoach/ViewControllerRoster.swift
@@ -1,29 +1,53 @@
-//
-//  ViewControllerRoster.swift
-//  BasketBossCoach
-//
-//  Created by Noel Ortiz on 11/15/23.
-//
 
 import UIKit
-
-class ViewControllerRoster: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+struct Player{
+    var name = ""
+    var number = 0
+}
+class AppData{
+    //static because only one will need to exist
+    static var players = [Player]()
+}
+class ViewControllerRoster: UIViewController,UITableViewDelegate,UITableViewDataSource {
+        
+    @IBOutlet weak var nameOut: UITextField!
+    @IBOutlet weak var rostaView: UITableView!
+    @IBOutlet weak var numberOut: UITextField!
+    
+    
+    let defaults = UserDefaults.standard
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            rostaView.delegate = self
+             rostaView.dataSource = self
+            if  let n = defaults.string(forKey: "TheSquad"){
+             
+                for p in AppData.players{
+                    print(p)
+                }
+                AppData.players.reloadData()
+            }
+            
+        }
+        
+    @IBAction func submitButt(_ sender: UIButton) {
+        var namee = nameOut.text!
+        var num = Int(numberOut.text!)!
+      var yay =  AppData.players.append(Player(name: namee,number: num))
+        defaults.set(yay, forKey: "TheSquad")
+//        shoppigList.reloadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        AppData.players.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "\(AppData.players[indexPath.row])"
+        return cell
+    }
 
-}
+    }
+
